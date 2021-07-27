868589897494bb09a5eadde975fba44e3d9f412d
Sahil Yadav <sahil.yadav@hcl.com>
2021-07-27 11:59:32 -0400

    intial commit

---
+++ dayonetopic/src/main/java/com/hcl/ojttraining_dayone_julytwse/dayonetopic/App.java
@@ -0,0 +1,13 @@
+package com.hcl.ojttraining_dayone_julytwse.dayonetopic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
